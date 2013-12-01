dac63d2923db2a5ba37c4531d1acba050cd79520
Avenger7x <avenger7x13@gmail.com>
2013-12-01 12:02:00 +0200

    pipe and compose function

--- fn.js
+++ fn.js
@@ -5,3 +5,21 @@ function memoize(fn, memo){
 		return memo[arg] || (memo[arg] = fn(arg));
 	};
 }
+
+function compose(fnA, fnB){
+	return function(){
+		return fnA(fnB.apply(null, arguments));
+	};
+}
+
+function pipe(){
+	var fns = Array.prototype.slice.call(arguments);
+
+	return function(data){
+		while(fns.length){
+			data = fns.shift()(data);
+		}
+
+		return data;
+	};
+}
