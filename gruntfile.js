b29d6c16b8fe9445fb7decf1140784e939c92c34
Avenger7x <avenger7x13@gmail.com>
2013-12-01 12:14:47 +0200

    gruntfile setup

---
+++ gruntfile.js
@@ -0,0 +1,39 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		bump: {
+			options: {
+				files: ['package.json', 'bower.json'],
+				commitFiles: ['-a'],
+				commitMessage: 'release %VERSION%',
+				tagMessage: 'version %VERSION%',
+				pushTo: 'origin'
+			}
+		},
+		jshint: {
+			codebase: {
+				src: 'fn.js'
+			}
+		},
+		complexity: {
+			codebase: {
+				src: 'fn.js'
+			}
+		},
+		uglify: {
+			options: {
+				report: 'min'
+			},
+			codebase: {
+				src: 'fn.js',
+				dest: 'fn.min.js'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-bump');
+	grunt.loadNpmTasks('grunt-complexity');
+
+	grunt.registerTask('default', ['jshint', 'complexity', 'uglify']);
+};
